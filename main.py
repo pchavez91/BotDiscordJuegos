19ba2ee87510c46c7961ab4afe06f2ceb93f191b
PatricioChavez <pchavez.dev@gmail.com>
2025-07-24 11:01:02 -0400

    primer commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.intents.default()
+intents.message_content = True
+
+bot = commands.Bot(commands_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Conectado como (bot.user.name)")
+
+@bot.command()
+async def hola(ctx):
+    await ctx.send("Hola soy tu bot de juegos gratis")
+
+bot.run(TOKEN)
